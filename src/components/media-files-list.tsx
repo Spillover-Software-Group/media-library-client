a267d1fd21761b8a27539904331f28aa8b7e785b
mindas-v <mvainauskas@spillover.com>
2022-01-20 17:42:26 +0000

    Added modal component for the addition of new folders. Still need to populate with actual addition form...

--- src/components/media-files-list.tsx
+++ src/components/media-files-list.tsx
@@ -1,18 +1,23 @@
 import * as React from 'react';
 import axios from 'axios';
 import './styles.scss';
+import Modal from '../utils/modal';
 
 interface Props{
     handleSelected: Function 
 }
 
+const baseUrl = "http://localhost:3030/";
+
 const MediaList: React.FC<Props> = ({handleSelected}:Props) => {    
     const [mediaList, setMediaList] = React.useState<any[]>([]);
+    const [foldersList, setFoldersList] = React.useState<any[]>([]);
+    const [addNewIsOpen, setAddNewIsOpen] = React.useState<boolean>(false)
 
     React.useEffect(() => {
         const getMediaList = async () => {
             // change for server url once API is hosted.
-            const filesResponse = await axios.get("http://localhost:3030/files");
+            const filesResponse = await axios.get(baseUrl + "files");
             if (filesResponse.status !== 200) {
                 console.log("DEBUG_ERROR_DURING_FILE_RETRIEVAL: ", filesResponse);
                 return [];
@@ -20,7 +25,13 @@ const MediaList: React.FC<Props> = ({handleSelected}:Props) => {
             console.log("MEDIA_LIST: ", filesResponse.data);
             return setMediaList(filesResponse.data);
         }
+        const getFoldersList = async () => {
+            const foldersResponse = await axios.get(baseUrl+"folders_list");
+            console.log("DEBUG_FOLDERS_LIST: ", foldersResponse);
+            return setFoldersList(foldersResponse.data);
+        }
         getMediaList();
+        getFoldersList();
         return;
     }, []);
 
@@ -28,14 +39,18 @@ const MediaList: React.FC<Props> = ({handleSelected}:Props) => {
         console.log("DEBUG_file: ", (e.target as any).src);
         handleSelected(e);   
     }
+
+    const handleFolderAddNewClick = () => {
+        setAddNewIsOpen(!addNewIsOpen);
+        console.log("DEBUG_FOLDER_ADD_NEW_OPEN_STATE: ", addNewIsOpen);
+    }
+
     console.log("DEBUG_LIST: ", mediaList);
     const filesList = mediaList.map(item => {
-        console.log("DEBUG_ITEM: ", item);
 
         return Object.entries(item).map(([key, value]) => {
             if (key === 'url') {
-                const mediaSrc = value as any as string;     // type as anytype then set as string to keep mediaSrc from causing complaints with img tag src prop.          
-                console.log("DEBUG_MEDIA_SRC: ", mediaSrc);
+                const mediaSrc = value as any as string;     // type as anytype then set as string to keep mediaSrc from causing complaints with img tag src prop.   
                 const fileName:string = item['filename'].split('.')[0];
                 return (
                     <div className="image-holder" id={`file-template-${item['id']}`} key={`file-template-${item['id']}`}>
@@ -48,14 +63,25 @@ const MediaList: React.FC<Props> = ({handleSelected}:Props) => {
         })
     })
 
-    console.log("DEBUG_FILES_LIST: ", filesList);
+    const folderNameList = foldersList.map(f => {
+        return(<div>{f.folderName || ''}</div>)
+    })
+
+    const displayModal = (
+        <Modal 
+            modalTitle={"Add New Folder"}
+            handleClose={() => setAddNewIsOpen(false)}>
+            <div>Add New Folder Form goes here...</div>
+        </Modal>
+    )
 
    return(
        <div className="files-list-container">
            <div className="folder-section">
-               <div className='folder-item'>{"Folder/Favourites?<<--WIP"}</div>
-               <div className='folder-item add-new'>Add New+</div>
+               <div className='folder-item add-new' onClick={handleFolderAddNewClick}>Add New</div>
+               <div className='folder-item'>{folderNameList}</div>
             </div>
+            {addNewIsOpen ? displayModal : null}
            <div className="files-list">{filesList}</div>
        </div>
    ) ;
