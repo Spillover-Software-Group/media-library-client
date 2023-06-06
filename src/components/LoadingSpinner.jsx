b9937523f609276bb07fd4702f6cf6428c1b52c2
Gustavo Camello <53873879+gustavo-camello@users.noreply.github.com>
2023-06-06 17:36:32 +0100

    Display a Loading message while loading files (#5)

---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,10 @@
+function LoadingSpinner() {
+  return (
+    <div
+      style={{ borderTopColor: "transparent" }}
+      className="sml-w-10 sml-h-10 sml-border-[3px] sml-border-solid sml-rounded-full sml-animate-spin"
+    ></div>
+  );
+}
+
+export default LoadingSpinner;
